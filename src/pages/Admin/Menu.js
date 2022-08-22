4b353bf08d9c6225de93eec209461c8c5003e093
Muhammad Bagus Syahputra Tambunan <bagus.tambunan@shopee.com>
2022-08-23 00:00:12 +0700

    refactor: change menu link

--- src/pages/Admin/Menu.js
+++ src/pages/Admin/Menu.js
@@ -11,54 +11,69 @@ export default function Menu() {
       </a>
       <hr />
       <ul class="nav nav-pills flex-column">
+
         <li class="nav-item">
-          <a href="#" class="nav-link active">
+          <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="false">
+            <i className="bi bi-house me-2"></i>
             Home
-          </a>
+          </button>
+          <div class="collapse" id="home-collapse">
+            <ul class="btn-toggle-nav list-unstyled small">
+              <li><a href="#" class="nav-link link-dark rounded"><i className="bi bi-chevron-right me-2"></i>Home</a></li>
+              <li><a href="#" class="nav-link link-dark rounded"><i className="bi bi-chevron-right me-2"></i>Profile</a></li>
+            </ul>
+          </div>
         </li>
+
+        <li class="nav-item">
+          <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#post-collapse" aria-expanded="false">
+            <i class="bi bi-newspaper me-2"></i>
+            Posts
+          </button>
+          <div class="collapse" id="post-collapse">
+            <ul class="btn-toggle-nav list-unstyled small">
+              <li><a href="#" class="nav-link link-dark rounded"><i className="bi bi-chevron-right me-2"></i>All posts</a></li>
+              <li><a href="#" class="nav-link link-dark rounded"><i className="bi bi-chevron-right me-2"></i>Tiers</a></li>
+              <li><a href="#" class="nav-link link-dark rounded"><i className="bi bi-chevron-right me-2"></i>Categories</a></li>
+            </ul>
+          </div>
+        </li>
+
+        <li class="nav-item">
+          <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#subscription-collapse" aria-expanded="false">
+            <i class="bi bi-card-list me-2"></i>
+            Subscriptions
+          </button>
+          <div class="collapse" id="subscription-collapse">
+            <ul class="btn-toggle-nav list-unstyled small">
+              <li><a href="#" class="nav-link link-dark rounded"><i className="bi bi-chevron-right me-2"></i>Subscriptions</a></li>
+              <li><a href="#" class="nav-link link-dark rounded"><i className="bi bi-chevron-right me-2"></i>Transactions</a></li>
+              <li><a href="#" class="nav-link link-dark rounded"><i className="bi bi-chevron-right me-2"></i>Vouchers</a></li>
+            </ul>
+          </div>
+        </li>
+
+        <li class="nav-item">
+          <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#gift-collapse" aria-expanded="false">
+          <i class="bi bi-gift me-2"></i>
+            Gifts
+          </button>
+          <div class="collapse" id="gift-collapse">
+            <ul class="btn-toggle-nav list-unstyled small">
+              <li><a href="#" class="nav-link link-dark rounded"><i className="bi bi-chevron-right me-2"></i>Gifts</a></li>
+              <li><a href="#" class="nav-link link-dark rounded"><i className="bi bi-chevron-right me-2"></i>Gift claims</a></li>
+            </ul>
+          </div>
+        </li>
+
         <li class="nav-item">
           <a href="#" class="nav-link link-dark">
-            Posts
+            <i class="bi bi-arrow-bar-left me-2"></i>
+            Logout
           </a>
         </li>
+
       </ul>
-      <hr />
-      <div class="dropdown admin-menu-user">
-        <a
-          href="#"
-          class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle"
-          id="dropdownUser"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-        >
-          <img
-            src="https://github.com/mdo.png"
-            alt=""
-            width="32"
-            height="32"
-            class="rounded-circle me-2"
-          />
-          <strong>mdo</strong>
-        </a>
-        <ul
-          class="dropdown-menu text-small shadow"
-          aria-labelledby="dropdownUser"
-        >
-          <li>
-            <a class="dropdown-item" href="#">
-              Profile
-            </a>
-          </li>
-          <li>
-            <hr class="dropdown-divider" />
-          </li>
-          <li>
-            <a class="dropdown-item" href="#">
-              Sign out
-            </a>
-          </li>
-        </ul>
-      </div>
     </div>
   );
 }
