2aa2d8157614b9abef5ab97574dfecff6291dd11
Muhammad Bagus Syahputra Tambunan <bagus.tambunan@shopee.com>
2022-08-23 01:46:17 +0700

    rafactor: change axios to fetch in login feature

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,68 +1,69 @@
-/* eslint-disable */
-import React, { useState } from 'react';
-import axios from 'axios';
-import LoginForm from '../components/LandingPage/LoginForm';
+import React, { useState } from "react";
+import LoginForm from "../components/LandingPage/LoginForm";
 
 export default function LandingPage() {
+  // const [isLoading, setIsLoading] = useState(true);
   const [form, setForm] = useState({
-    email: 'jean@mail.com',
-    password: 'jean123',
+    email: "jean@mail.com",
+    password: "jean123",
   });
   const handleChange = (e) => {
     const { name } = e.currentTarget;
     const { value } = e.currentTarget;
     setForm({ ...form, [name]: value });
   };
-  const submitForm = () => {
-    if (isFormValid()) {
-      getToken();
-    }
-  };
   const isFormValid = () => {
-    if (form.email === '') {
+    if (form.email === "") {
       return false;
     }
-    if (form.password === '') {
+    if (form.password === "") {
       return false;
     }
     return true;
   };
 
-  const getToken = () => {
-    axios.post(
-      'http://localhost:8080/sign-in',
-      {
-        email: form.email,
-        password: form.password,
-      },
-      {
-        'Access-Control-Allow-Origin': '*',
-      }
-    )
+  const fetchToken = () => {
+    const dataToPost = {
+      email: form.email,
+      password: form.password,
+    };
+
+    fetch("http://localhost:8080/sign-in", {
+      method: "POST",
+      body: JSON.stringify(dataToPost),
+    })
+      .then((res) => res.json())
       .then((res) => {
-        if(res.data.statusCode === 200) {
-          setToken(res.data.data.idToken);
-        } else {
-          console.log(res.data);
+        if (!res.error){
+          console.log(res.data.token);
+          setToken(res.data.token);
         }
       })
-      .catch((error) => {
-        console.log(error);
+      .catch((err) => {
+        console.log(err);
       });
   };
 
+  const submitForm = () => {
+    if (isFormValid()) {
+      fetchToken();
+    }
+  };
+
+  const getToken = () => localStorage.getItem("token");
   const setToken = (token) => {
     localStorage.setItem('token',token);
     console.log('Login success');
   };
+  if (getToken() !== null) {
+    return "ANDA SUDAH LOGIN";
+  }
 
   return (
-    <main className="">
-      <LoginForm
-        form={form}
-        handleChange={handleChange}
-        submitForm={submitForm}
-      />
-    </main>
+    <LoginForm
+      form={form}
+      handleChange={handleChange}
+      submitForm={submitForm}
+    />
   );
 }
