58b3dba13c96ca2de40c27068feebc4d420e9dd1
Muhammad Bagus Syahputra Tambunan <bagus.tambunan@shopee.com>
2022-08-26 15:18:17 +0700

    feat: create token reducer

---
+++ src/store/tokenSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  token: '',
+};
+
+export const tokenSlice = createSlice({
+  name: 'token',
+  initialState,
+  reducers: {
+    setToken: (state, action) => {
+      state.value = action.payload;
+      localStorage.setItem('token', action.payload);
+    },
+    removeToken: () => {
+      localStorage.removeItem('token');
+    },
+  },
+});
+
+export const { setToken, removeToken } = tokenSlice.actions;
+export const selectToken = (state) => state.token.value;
+export default tokenSlice.reducer;
