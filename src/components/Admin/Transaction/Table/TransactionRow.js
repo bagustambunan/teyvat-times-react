ab4929d61431ec24fdeaab9c7065a47c30798a27
Muhammad Bagus Syahputra Tambunan <bagus.tambunan@shopee.com>
2022-08-29 20:28:27 +0700

    feat: admin can approve or reject a transaction

--- src/components/Admin/Transaction/Table/TransactionRow.js
+++ src/components/Admin/Transaction/Table/TransactionRow.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import { ParseDate } from '../../../../helpers/Parser';
 
 export default function TransactionRow({ transaction, i, handleDelete }) {
@@ -10,12 +11,12 @@ export default function TransactionRow({ transaction, i, handleDelete }) {
       <td>{transaction.subscription.name}</td>
       <td>{transaction.status.name}</td>
       <td>
-        <a
-          href={`/admin/transactions/${transaction.transactionID}`}
+        <NavLink
+          to={`/admin/transactions/${transaction.transactionID}`}
           className="btn btn-sm"
         >
           <i className="text-primary bi bi-eye-fill" />
-        </a>
+        </NavLink>
       </td>
     </tr>
   );
--- src/components/Admin/Transaction/TransactionDetail.js
+++ src/components/Admin/Transaction/TransactionDetail.js
@@ -1,38 +1,96 @@
-import React from 'react'
-import { ParseCurrency, ParseDate } from '../../../helpers/Parser'
+import React from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { ParseCurrency, ParseDate } from "../../../helpers/Parser";
+import { selectToken } from "../../../store/tokenSlice";
 
 export default function TransactionDetail({ transaction }) {
   console.log(transaction);
+  const token = useSelector(selectToken);
+  const navigate = useNavigate();
+  const approveTransaction = (transactionID) => {
+    fetch('http://localhost:8080/transactions/'+transactionID+'/approve', {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.statusCode === 200) {
+          toast.success('Transaction approved');
+          navigate('/admin/transactions');
+        }
+        if (res.statusCode !== 200) {
+          toast.error(`Error: ${res.message}`);
+        }
+      })
+      .catch((err) => {
+        toast.error(`Error: ${err.message}`);
+      });
+  };
+  const rejectTransaction = (transactionID) => {
+    fetch('http://localhost:8080/transactions/'+transactionID+'/reject', {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(dataToPost),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.statusCode === 200) {
+          toast.success('Transaction rejected');
+          navigate('/admin/transactions');
+        }
+        if (res.statusCode !== 200) {
+          toast.error(`Error: ${res.message}`);
+        }
+      })
+      .catch((err) => {
+        toast.error(`Error: ${err.message}`);
+      });
+  };
+
   return (
     <div>
       <table className="table">
-          <tbody>
-            <tr>
-              <th scope="row">Date</th>
-              <td>{ParseDate(transaction.createdAt)}</td>
-            </tr>
-            <tr>
-              <th scope="row">User</th>
-              <td>
-                {transaction.user.name}
-              </td>
-            </tr>
-            <tr>
-              <th scope="row">Subscription</th>
-              <td>
-                {transaction.subscription.name}
-              </td>
-            </tr>
-            <tr>
-              <th scope="row">Subtotal</th>
-              <td>{ParseCurrency(transaction.grossTotal)}</td>
-            </tr>
-            <tr>
-              <th scope="row">Total</th>
-              <td>{ParseCurrency(transaction.netTotal)}</td>
-            </tr>
-          </tbody>
-        </table>
+        <tbody>
+          <tr>
+            <th scope="row">Date</th>
+            <td>{ParseDate(transaction.createdAt)}</td>
+          </tr>
+          <tr>
+            <th scope="row">Status</th>
+            <td>{transaction.status.name}</td>
+          </tr>
+          <tr>
+            <th scope="row">User</th>
+            <td>{transaction.user.name}</td>
+          </tr>
+          <tr>
+            <th scope="row">Subscription</th>
+            <td>{transaction.subscription.name}</td>
+          </tr>
+          <tr>
+            <th scope="row">Subtotal</th>
+            <td>{ParseCurrency(transaction.grossTotal)}</td>
+          </tr>
+          <tr>
+            <th scope="row">Total</th>
+            <td>{ParseCurrency(transaction.netTotal)}</td>
+          </tr>
+        </tbody>
+      </table>
+      {transaction.status.transactionStatusID === 2 ? (
+        <div>
+          <button onClick={() => approveTransaction(transaction.transactionID)} type="button">Approve</button>
+          <button onClick={() => rejectTransaction(transaction.transactionID)} type="button">Reject</button>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
-  )
+  );
 }
