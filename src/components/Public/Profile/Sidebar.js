33ee25a7aac6facdb028ed7b0b3c74a5cc28c641
Muhammad Bagus Syahputra Tambunan <bagus.tambunan@shopee.com>
2022-08-30 23:02:06 +0700

    refactor: change style in profile layout

--- src/components/Public/Profile/Sidebar.js
+++ src/components/Public/Profile/Sidebar.js
@@ -3,54 +3,54 @@ import { NavLink } from 'react-router-dom';
 
 export default function Sidebar() {
   return (
-    <div className="d-flex flex-column p-3">
-      <ul className="nav nav-pills flex-column">
+    <div className="d-flex p-3 profile-sidebar">
+      <ul className="nav nav-pills">
         <li className="nav-item">
           <NavLink to="/profile/account" className="nav-link link-dark">
             <i className="bi bi-person me-2" />
-            Account
+            <span>Account</span>
           </NavLink>
         </li>
 
         <li className="nav-item">
           <NavLink to="/profile/mytransaction" className="nav-link link-dark">
             <i className="bi bi-credit-card me-2" />
-            My Transaction
+            <span>My Transaction</span>
           </NavLink>
         </li>
 
         <li className="nav-item">
           <NavLink to="/profile/mysubscription" className="nav-link link-dark">
             <i className="bi bi-card-list me-2" />
-            My Subscription
+            <span>My Subscription</span>
           </NavLink>
         </li>
 
         <li className="nav-item">
           <NavLink to="/profile/myvoucher" className="nav-link link-dark">
             <i className="bi bi-ticket-perforated me-2" />
-            My Voucher
+            <span>My Voucher</span>
           </NavLink>
         </li>
 
         <li className="nav-item">
           <NavLink to="/profile/myreferral" className="nav-link link-dark">
             <i className="bi bi-link-45deg me-2" />
-            My Referral
+            <span>My Referral</span>
           </NavLink>
         </li>
 
         <li className="nav-item">
           <NavLink to="/profile/mygift" className="nav-link link-dark">
             <i className="bi bi-gift me-2" />
-            My Gift
+            <span>My Gift</span>
           </NavLink>
         </li>
 
         <li className="nav-item">
           <NavLink to="/logout" className="nav-link link-dark">
             <i className="bi bi-arrow-bar-left me-2" />
-            Logout
+            <span>Logout</span>
           </NavLink>
         </li>
       </ul>
--- src/pages/Public/Profile/ProfileLayout.js
+++ src/pages/Public/Profile/ProfileLayout.js
@@ -4,12 +4,12 @@ import Sidebar from '../../../components/Public/Profile/Sidebar';
 
 export default function ProfileLayout() {
   return (
-    <div className="card border rounded shadow bg-light p-3">
+    <div className="border rounded shadow bg-light p-3 profile-layout">
       <div className="row">
         <div className="col-12 col-md-3">
           <Sidebar />
         </div>
-        <div className="col-12 col-md-9 border-start border-2 p-4">
+        <div className="profile-main col-12 col-md-9 p-4">
           <Outlet />
         </div>
       </div>
